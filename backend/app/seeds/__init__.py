20797fce7e8a2677a5970505e6d4a3cef7b6431d
mikaelkuniko@gmail.com <mikaelkuniko@Mikaels-Macbook.local>
2023-01-14 20:53:07 -0800

    Finished seeders with the exception of join tables and updated .gitignore file

--- backend/app/seeds/__init__.py
+++ backend/app/seeds/__init__.py
@@ -1,6 +1,9 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .types import seed_types, undo_types
 from .businesses import seed_businesses, undo_businesses
+from .amenities import seed_amenities, undo_amenities
+from .reviews import seed_reviews, undo_reviews
 
 from app.models.db import db, environment, SCHEMA
 
@@ -13,20 +16,41 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
     seed_users()
     # Add other seed functions here
 
-# businesses 
+# types
+@seed_commands.command('all')
+def seed():
+    if environment == 'production':
+        undo_types()
+    seed_types()
+
+# businesses
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
         undo_businesses()
     seed_businesses()
+
+# amenities
+@seed_commands.command('all')
+def seed():
+    if environment == 'production':
+        undo_amenities()
+    seed_amenities()
+
+# reviews
+@seed_commands.command('all')
+def seed():
+    if environment == 'production':
+        undo_reviews()
+    seed_reviews()
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
@@ -34,8 +58,22 @@ def undo():
     undo_users()
     # Add other undo functions here
 
+# types
+@seed_commands.command('undo')
+def undo():
+    undo_types()
+
 # businesses
 @seed_commands.command('undo')
 def undo():
     undo_businesses()
-    
+
+# amenities
+@seed_commands.command('undo')
+def undo():
+    undo_amenities()
+
+# reviews
+@seed_commands.command('undo')
+def undo():
+    undo_reviews()
