68666e35ec2b8e527ce422a451c87723879623ae
marc <marcsmithr@gmail.com>
2023-01-17 15:41:09 -0600

    business reducer created

---
+++ frontend/src/store/businessReducer.js
@@ -0,0 +1,7 @@
+import { csrfFetch } from "./csrf"
+
+const LOAD = 'spots/LOAD'
+const CREATE = 'spots/CREATE'
+const DELETE = 'spots/DELETE'
+const GET_ONE = 'spots/GET_ONE'
+const CREATE_IMG = 'spots/image/CREATE'
