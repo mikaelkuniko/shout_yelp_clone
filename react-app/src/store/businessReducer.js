1cfcb8e71b19f0fb0acfbb2b99583ea79a3860f7
mikaelkuniko@gmail.com <mikaelkuniko@Mikaels-Macbook.local>
2023-01-18 12:55:14 -0800

    Added business reducers

--- react-app/src/store/businessReducer.js
+++ react-app/src/store/businessReducer.js
@@ -1,5 +1,3 @@
-import { csrfFetch } from "./csrf"
-
 const LOAD = 'bizes/LOAD'
 const CREATE = 'bizes/CREATE'
 const DELETE = 'bizes/DELETE'
@@ -30,3 +28,38 @@ const remove = bizId => ({
     type: DELETE,
     bizId
 })
+
+export const getOneSpot = (id) => async dispatch => {
+    const response = await fetch(`/api/biz/${id}`);
+    if (response.ok){
+        const business = await response.json();
+        dispatch(getOne(business))
+        return business
+    }
+    return response
+}
+
+
+
+
+
+const initialState = {allBusinesses: {}, singleBusiness: {}}
+
+const businessReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type){
+        case GET_ONE: {
+            newState = {
+                ...state,
+                allBusinesses: {...state.allBusinesses},
+                singleBusiness: {}
+            }
+            newState.singleBusiness = action.spot
+            return newState
+        }
+        default:
+            return state
+    }
+}
+
+export default businessReducer
