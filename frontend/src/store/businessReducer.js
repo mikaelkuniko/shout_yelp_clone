5d88a379343d5b823ee854d6acf0eb6894c8b1cc
marc <marcsmithr@gmail.com>
2023-01-17 18:32:49 -0600

    merged businessFrontend with dev

--- frontend/src/store/businessReducer.js
+++ frontend/src/store/businessReducer.js
@@ -1,7 +1,32 @@
 import { csrfFetch } from "./csrf"
 
-const LOAD = 'spots/LOAD'
-const CREATE = 'spots/CREATE'
-const DELETE = 'spots/DELETE'
-const GET_ONE = 'spots/GET_ONE'
-const CREATE_IMG = 'spots/image/CREATE'
+const LOAD = 'bizes/LOAD'
+const CREATE = 'bizes/CREATE'
+const DELETE = 'bizes/DELETE'
+const GET_ONE = 'bizes/GET_ONE'
+const CREATE_IMG = 'bizes/image/CREATE'
+
+const load = bizes => ({
+    type: LOAD,
+    bizes
+})
+
+const create = biz => ({
+    type: CREATE,
+    biz
+})
+
+const createImage = image => ({
+    type: CREATE,
+    image
+})
+
+const getOne = biz => ({
+    type: GET_ONE,
+    biz
+})
+
+const remove = bizId => ({
+    type: DELETE,
+    bizId
+})
