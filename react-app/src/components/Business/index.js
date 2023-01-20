761ba57eb52fc9fc00556e472757c3e80e83f279
Joyce <joyceyukang@gmail.com>
2023-01-20 11:06:33 -0800

    user favorites bugs fixed

--- react-app/src/components/Business/index.js
+++ react-app/src/components/Business/index.js
@@ -10,19 +10,24 @@ const BusinessDetails = () => {
     const dispatch = useDispatch()
     const { businessId } = useParams()
     const [bookMark, setBookMark] = useState(false)
-
+    
     const currentUser = useSelector(state => state.session.user)
-    const userBusinesses = currentUser.user_businesses
+    let  userBusinesses;
 
+    if (currentUser) {
+        userBusinesses = currentUser.user_businesses
+    }
+
+    
     // console.log('BUSINESS ID FROM PARAMS', businessId)
     const business = useSelector((state)=> state.businesses.singleBusiness)
     const totalReviews = Object.values(useSelector((state)=> state.reviews.allReviews))
     // console.log('BUSINESS FROM USE SELECTOR', business)
-
+    
     const bizReviews = totalReviews.filter((review) => Number(businessId) === Number(review.business_id))
-
-     // When the bookmark is filled
-     const handleDelete = async (e) => {
+    
+    // When the bookmark is filled
+    const handleDelete = async (e) => {
         e.preventDefault();
         setBookMark(!bookMark)
         const response = await fetch(`/api/users/${businessId}/favorite`, {
@@ -32,7 +37,7 @@ const BusinessDetails = () => {
         const message = await response.json();
         if(message) dispatch(authenticate())
     };
-
+    
     // When the bookmark is unfilled
     const handleAdd = async (e) => {
         e.preventDefault();
@@ -44,21 +49,23 @@ const BusinessDetails = () => {
         const message = await response.json();
         if(message) dispatch(authenticate())
     };
-
+    
     useEffect(()=>{
         dispatch(getOneBusiness(businessId))
     }, [dispatch, bizReviews.length, businessId])
-
+    
     useEffect(() => {
-        for(let i = 0; i < userBusinesses.length - 1; i++) {
-            if (Number(userBusinesses[i].id) === Number(businessId)) {
-                setBookMark(true)
+        if(userBusinesses) {
+            for(let i = 0; i < userBusinesses.length - 1; i++) {
+                if (Number(userBusinesses[i].id) === Number(businessId)) {
+                    setBookMark(true)
+                }
             }
         }
     }, [])
-
+    
     if(!business.name) return null
-
+    
     else return (
         <div>
             <ul>
@@ -69,7 +76,8 @@ const BusinessDetails = () => {
                 <li>{business.review_avg}</li>
                 {/* <li>Rating: {avgRating}</li> */}
                 <Link to={`/biz/${businessId}/writeareview`}>Write a Review</Link>
-                <>
+                { currentUser && 
+                (<>
                     {bookMark ? 
                         <button onClick={handleDelete}>
                             <i className="fa-solid fa-bookmark"></i>
@@ -79,7 +87,8 @@ const BusinessDetails = () => {
                            <i className="fa-regular fa-bookmark"></i>
                         </button>
                     }
-                </>
+                </>)
+                }
                 <div className="reviews">
                     {bizReviews.map((review) => (
                         <BusinessReviews key={review.id} {...review}/>
