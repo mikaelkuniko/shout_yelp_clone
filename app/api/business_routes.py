a556c703bd51aa5ff0bed9065fc518eeccb3a619
marc <marcsmithr@gmail.com>
2023-01-20 11:53:12 -0600

    pre-merge commit

--- app/api/business_routes.py
+++ app/api/business_routes.py
@@ -12,6 +12,7 @@ business_routes = Blueprint('business', __name__)
 # Search businesses
 @business_routes.route('/search', methods=['GET'])
 def search():
+    print('HELLO FROM BACKEND')
     business_search = ""
     location_search = ""
     form = Search_Form()
@@ -20,32 +21,55 @@ def search():
         "errors": form.errors
         }
     args = request.args
+    print("ARGS IN BACKEND", args)
     if "business" in args:
         business_search = args.get('business')
     if "location" in args:
         location_search = args.get('location')
-
+        print("LOCATION IN BACKEND", location_search)
+    business_id_list = []
     business_query = []
     location_query = []
     businesses = []
+    business_set = set()
+    if business_search:
+        business_list = business_search.split(' ')
+        for business_args in business_list:
+            business_set.add(business_args)
 
     if business_search:
-        business_query.append(Business.name.like(f'%{business_search}%'))
+        for business_search_params in business_set:
+            business_query.append(Business.name.like(f'%{business_search_params}%'))
     if location_search:
         location_query.append(Business.city.ilike(f'%{location_search}%'))
         location_query.append(Business.zip_code.ilike(f'%{location_search}%'))
         location_query.append(Business.state.ilike(f'%{location_search}%'))
+        print('LOCATION QUERY', location_query)
+        for l_query in location_query:
+            # the comma in the filter params is an AND statement
+            if business_query:
+                for b_query in business_query:
+                    business = Business.query.filter(b_query, l_query).all()
+                    if business:
+                        for biz in business:
+                            if biz.id not in business_id_list:
+                                businesses.append(biz)
+                                business_id_list.append(biz.id)
+            else:
+                business = Business.query.filter(l_query).all()
+                if business:
+                        for biz in business:
+                            if biz.id not in business_id_list:
+                                businesses.append(biz)
+                                business_id_list.append(biz.id)
 
-        for query in location_query:
-            business = Business.query.filter(*business_query, query).all()
-            if business:
-                for biz in business:
-                    businesses.append(biz)
     else:
-        business = Business.query.filter(*business_query).all()
-        if business:
-            for biz in business:
-                businesses.append(biz)
+       for b_query in business_query:
+                business = Business.query.filter(b_query).all()
+                if business:
+                    for biz in business:
+                        if biz.id not in business_id_list:
+                            businesses.append(biz)
 
     print('businesses', businesses)
 
