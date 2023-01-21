bff81106ae04d03accdf981d577ee448e3efb2bb
brandon <brandon.bim13@gmail.com>
2023-01-20 19:16:22 -0500

    added css for reviews

--- react-app/src/components/Reviews/ReviewDetails.js
+++ react-app/src/components/Reviews/ReviewDetails.js
@@ -3,12 +3,30 @@ import { Link } from 'react-router-dom'
 import './ReviewDetails.css'
 
 function ReviewDetails(review) {
+    let noFill = <i className="fa-solid fa-star stars noFill" />
+    let fill = <i className="fa-solid fa-star stars fill" />
+    const stars = []
+
+    for(let i = 0; i < 5; i++){
+        if (i < review.stars) stars.push(fill)
+        else stars.push(noFill)
+    }
     if(!review) return null
     return (
         <div className='reviewCard'>
             <Link to={`/biz/${review.business_id}`}>
-                <h4>User id: {review.user.id} --------- Stars: {review.stars}</h4>
-                <p>Review: {review.review}</p>
+                <div className='userInfo'>
+                    {review.user.profile_pic !== null ? <img src={review.user.profile_pic} alt='profile_pic' /> : <i className="fa-regular fa-user pic"></i> }
+                    <div>
+                        <h4>{review.user.username}</h4>
+                    </div>
+                </div>
+                <div className='reviewRating'>
+                {stars.map((star, i) => (
+                    <span key={i}>{star}</span>
+                    ))}
+                </div>
+                <p className='reviewBody'>Review: {review.review}</p>
                 <div>
                     <span>Useful: {review.useful} </span>
                     <span>Cool: {review.cool} </span>
