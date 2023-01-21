bc98b2d32911ac3728ca308a5b050e3202985b88
Joyce <joyceyukang@gmail.com>
2023-01-21 14:55:56 -0800

    user bookmarks css finished

--- react-app/src/components/User/FavoriteBusinesses.js
+++ react-app/src/components/User/FavoriteBusinesses.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux'
 import { authenticate } from '../../store/session';
+import { allReviews } from "../../store/review";
 import './index.css'
 
 function FavoriteBusinesses({ id, name, city, state, preview_image, num_reviews, sum_rating }) {
@@ -10,6 +11,29 @@ function FavoriteBusinesses({ id, name, city, state, preview_image, num_reviews,
 
     const currentUser = useSelector(state => state.session.user)
     const userBusinesses = currentUser.user_businesses
+
+    // Review average rating
+    const totalReviews = Object.values(useSelector((state)=> state.reviews.allReviews))
+
+    const bizReviews = totalReviews.filter((review) => Number(id) === Number(review.business_id))
+
+    let sum = 0;
+
+    for (let i = 0; i < bizReviews.length; i++) {
+        sum += bizReviews[i].stars
+    }
+
+    const avg = sum / bizReviews.length
+
+    //Star images
+    let noFill = <i className="fa-solid fa-star stars noFill" />
+    let fill = <i className="fa-solid fa-star stars fill" />
+    const stars = []
+
+    for (let i = 0; i < 5; i++) {
+        if (i < avg) stars.push(fill)
+        else stars.push(noFill)
+    }
 
 
     // When the bookmark is filled
@@ -36,6 +60,7 @@ function FavoriteBusinesses({ id, name, city, state, preview_image, num_reviews,
         dispatch(authenticate())
     };
 
+    //Update the state of users
     useEffect(() => {
         dispatch(authenticate())
     }, [dispatch])
@@ -61,27 +86,34 @@ function FavoriteBusinesses({ id, name, city, state, preview_image, num_reviews,
                                 </div>
                             </NavLink>
                         </>
-                    </div>
-                    <div className='middle-bar'>
-                        <>
-                            <NavLink key={id} to={`/biz/${id}`}>{name}</NavLink>
-                        </>
-                        <>
-                            {`${sum_rating} ${num_reviews}`}
-                        </>
-                        <>
-                            <NavLink key={name} to={`/biz/${id}`}>
-                                {`${city}, ${state}`}
-                            </NavLink>
-                        </>
+                        <div id='info'>
+                            <span id='name'>
+                                <NavLink key={id} to={`/biz/${id}`}>{name}</NavLink>
+                            </span>
+                            <span id='review-info'>
+                                {
+                                    stars.map((star, i) => (
+                                        <span key={i}>{star}</span>
+                                    ))
+                                }
+                                <span id='num-reviews'>
+                                    {`${num_reviews} reviews`}
+                                </span>
+                            </span>
+                            <span id='address'>
+                                <NavLink key={name} to={`/biz/${id}`}>
+                                    {`${city}, ${state}`}
+                                </NavLink>
+                            </span>
+                        </div>
                     </div>
                     <div className='right-bar'>
                         {bookMark ?
-                            <button onClick={handleDelete}>
+                            <button className='flags' onClick={handleDelete}>
                                 <i className="fa-solid fa-bookmark"></i>
                             </button>
                             :
-                            <button onClick={handleAdd}>
+                            <button className='flags' onClick={handleAdd}>
                                 <i className="fa-regular fa-bookmark"></i>
                             </button>}
                     </div>
