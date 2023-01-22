d995c63a857969744b7528c0a0aaa6566a451bb7
Joyce <joyceyukang@gmail.com>
2023-01-21 17:48:47 -0800

    edits to login, signup, and create a business forms

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
-import { useModal } from "../../context/Modal";
+import './index.css'
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -34,7 +34,7 @@ const LoginForm = () => {
     let demoEmail = 'demo@aa.io'
     let demoPw = 'password'
     const demo = await dispatch(login(demoEmail, demoPw))
-    if (demo){
+    if (demo) {
       setErrors(demo);
     }
   }
@@ -44,35 +44,42 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div className='outer-div'>
+      <h3>Login</h3>
+      <div className='user-input-div'>
+        <form className='user-input-form' onSubmit={onLogin}>
+          <div>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div>
+            <p htmlFor='email'>Email</p>
+            <input
+              name='email'
+              type='text'
+              placeholder='Email'
+              value={email}
+              onChange={updateEmail}
+            />
+          </div>
+          <div>
+            <p htmlFor='password'>Password</p>
+            <input
+              name='password'
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={updatePassword}
+            />
+          </div>
+          <div className='submit-button'>
+            <button type='submit'>Login</button>
+            <button className='single-login' onClick={demoUser}>Demo</button>
+          </div>
+        </form>
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-        <button className='single-login' onClick={demoUser}>Demo</button>
-      </div>
-    </form>
+    </div>
   );
 };
 
